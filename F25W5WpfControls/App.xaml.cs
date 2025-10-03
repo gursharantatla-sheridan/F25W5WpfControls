3c8ce7bdfdb926201781a16acf1b843cafb7833b
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-10-03 13:13:28 -0400

    Add project files.

---
+++ F25W5WpfControls/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace F25W5WpfControls
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
